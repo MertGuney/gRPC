4e4250acc3823b71d9504ae9276c26a1ebe98fc1
mertguney <guneeymert@outlook.com>
2023-06-11 14:25:25 +0300

    gRPC Example Initial

---
+++ gRPC.Client.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿using gRPC.Service;
+using Grpc.Net.Client;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7162");
+var greetClient = new Greeter.GreeterClient(channel);
+
+HelloReply response = await greetClient.SayHelloAsync(new HelloRequest() { Name = "Mert" });
+Console.WriteLine($"Response: {response.Message}");
