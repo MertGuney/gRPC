4f10c11fea5adc044cc8913aac7c9fc70ec4860f
mertguney <guneeymert@outlook.com>
2023-06-17 21:14:50 +0300

    User proto and Service Created

---
+++ gRPC.Service/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace gRPC.Service.Services;
+public class UserService : User.UserBase
+{
+    private readonly List<UserModel> _users = new();
+    public UserService()
+    {
+        _users.Add(new UserModel { Id = 1, Name = "Mert", Surname = "Guney" });
+    }
+
+    public override Task<Response> AddUser(AddUserRequest request, ServerCallContext context)
+    {
+        UserModel user = new()
+        {
+            Id = 2,
+            Name = request.Name,
+            Surname = request.Surname,
+        };
+        _users.Add(user);
+
+        return Task.FromResult(new Response() { Message = "User Added." });
+    }
+
+    public override async Task GetUsers(Empty request, IServerStreamWriter<UserModel> responseStream, ServerCallContext context)
+    {
+        foreach (var user in _users)
+        {
+            await responseStream.WriteAsync(user);
+        }
+    }
+}
+
